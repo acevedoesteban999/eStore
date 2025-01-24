973a7430f6a78dea885a2d6929de049ccddb36cb
Esteban <acevedoesteban999@gmail.com>
2025-01-23 23:03:09 -0500

    .

--- include/eStore.h
+++ include/eStore.h
@@ -16,7 +16,6 @@ void estore_write_float(const char* name, float value);
 void estore_write_string(const char* name, const char* value);
 esp_err_t estore_write_struct(const char* name, const void* _struct,size_t size);
 
-// Modificar declaraciones de funciones read
 bool estore_read_int8(const char *name, int8_t *value);
 bool estore_read_int32(const char *name, int32_t *value);
 bool estore_read_uint64(const char* name, uint64_t* value);
